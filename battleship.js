20ee85f89e4ac5d7f3e46a8c64d8b6b60776d36b
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-02-02 14:12:01 +0300

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -11,7 +11,7 @@ displayMiss: function(location){
 	var cell = document.getElementById(location);
 	cell.setAttribute("class", "miss");
 }	
-}
+};
 
 var model = {
 	boardSize: 7,
@@ -20,16 +20,42 @@ var model = {
 	shipsSunk: 0,
 	
 	ships: [
-	{locations: ["06", "16" "26"], hits: ["", "", ""]},
+	{locations: ["06", "16", "26"], hits: ["", "", ""]},
 		{locations: ["24", "34", "44"], hits: ["", "", ""]},
 			{locations: ["10", "11", "12"], hits: ["", "", ""]}
-	]
-}
+	],
+	
+	fire: function (guess) {
+		
+		for(var i = 0; i < this.numShips; i++){
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+			if(index >=0){
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+				if(this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!");
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+			view.displayMiss(guess);
+			view.displayMessage("You missed.");
+			return false;
+	},
+	
+	isSunk: function(ship) {
+		for(var i = 0; i < this.shipLength; i++){
+			if(ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+};
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Tap tap, is this thing on?");
+
+
