01fcb2b2800ed210e0cc6c149699a4fbfa70c370
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-02-03 18:13:30 +0300

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -57,30 +57,40 @@ var model = {
 	
 };
 
-var controller = {
-	guesses: 0,
-	
-	processGuess: function(guess){
-		
-	}
-};
-
-function parseGuess(guess) {
+function phraseGuess(guess) {
 	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 	if(guess === null || guess.length !== 2) {
 		alert("Please enter a letter and a number on the board");
 	} else {
-		firstChar = guess.charAt(0);
+		var firstChar = guess.charAt(0);
 		var row = alphabet.indexOf(firstChar);
 		var column = guess.charAt(1);
 		
-		if(isNan(row) || isNan(column) {
+		if(isNaN(row) || isNaN(column)) {
 			alert("That isn't on the board");
-		} else if (row < 0 || row >= model.boardSize || column <0 || column >= model.boardSize){
+		} else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
 			alert("That's off the board!");
 		} else {
 			return row + column;
 		}
-		return null;
 	}
+		return null;
 }
+
+
+var controller = {
+	guesses: 0,
+	
+	processGuess: function(guess){
+		var location = phraseGuess(guess);
+		if(location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+			}
+		}
+	}
+};
+
+
