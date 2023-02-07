af4bd214d6e2973926349ed7302e28fdeeeb9b7a
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-02-07 21:05:07 +0300

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -118,3 +118,26 @@ function handleFireButton () {
 }
 
 window.onload = init;
+
+generateShip: function() {
+	var direction = Math.floor(Math.random() * 2);
+	var row, col;
+	
+	if(direction === 1){
+		row = Math.floor(Math.random() * this.boardSize);
+			col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+	} else {
+		row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+			col = Math.floor(Math.random() * this.boardSize);
+	}
+	
+	var newShipLocations = [];
+	for(var i = 0; i < this.shipLength; i++) {
+		if (direction === 1) {
+			newShipLocations.push(row + "" + (col + i));
+		} else {
+			newShipLocations.push((row + i) + "" + col);
+		}
+	}
+	return newShipLocations;
+}
